d8c9b8cb03ab8f0264524b1baffbb318ecd007ea
Michel Promonet <michel.promonet@urbanandmainlines.com>
2025-11-20 00:01:20 +0100

    simplify ts

--- sae-track-power-section.ts
+++ sae-track-power-section.ts
@@ -2,11 +2,8 @@ import type { IAnimator, IAttribute, ISynopticContext } from "@ids/synoptic-lib"
 
 export class SaeTrackPowerSection implements IAnimator {
     public attributes: string[] = [];
-    private _entity!: SVGElement;
-    private _entityId!: string;
 
     private readonly _STATE_ATTR = ".powerState";
-    private _stateValue?: number;
 
     private readonly _UNKNOWN_STATE_CSS_CLASS = "sae-track-power-section-unknown";
     private readonly _ON_STATE_CSS_CLASS = "sae-track-power-section-on";
@@ -17,12 +14,9 @@ export class SaeTrackPowerSection implements IAnimator {
     }
 
     public dispose(): void {
-        // NOTHING TO DISPOSE
     }
 
     public initialize(entity: SVGElement, entityId: string, context: ISynopticContext): void {
-        this._entity = entity;
-        this._entityId = entityId;
     }
 
     onSelectionChange?(_selected: boolean, _entityId: string): boolean {
@@ -36,8 +30,7 @@ export class SaeTrackPowerSection implements IAnimator {
         const stateValue = data.get(this._STATE_ATTR);
 
         if (stateValue && typeof stateValue.value === "number") {
-            this._stateValue = stateValue.value;
-            this._updateGraphic(entity);
+            this._updateGraphic(entity, stateValue.value);
         } else {
             this._setUnknown(entity)
         }
@@ -47,12 +40,12 @@ export class SaeTrackPowerSection implements IAnimator {
         this._setUnknown(entity);
     }
 
-    private _updateGraphic(entity: SVGElement) {
-        if (this._stateValue !== undefined) {
-            if (this._stateValue === 0) {
+    private _updateGraphic(entity: SVGElement, stateValue: number): void {
+        if (stateValue !== undefined) {
+            if (stateValue === 0) {
                 entity.classList.remove(this._UNKNOWN_STATE_CSS_CLASS, this._ON_STATE_CSS_CLASS);
                 entity.classList.add(this._OFF_STATE_CSS_CLASS);
-            } else if (this._stateValue === 1) {
+            } else if (stateValue === 1) {
                 entity.classList.remove(this._UNKNOWN_STATE_CSS_CLASS, this._OFF_STATE_CSS_CLASS);
                 entity.classList.add(this._ON_STATE_CSS_CLASS);
             } else {
